d00dc7ea48fd20d3a76e6d772ed394c75e1ad835
ValeryManakov <valera.manakov.93@bk.ru>
2022-01-03 10:23:43 +0300

    2_add_restaurant_entity

---
+++ src/main/java/ru/javaops/topjava2/model/Restaurant.java
@@ -0,0 +1,14 @@
+package ru.javaops.topjava2.model;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "restaurants", uniqueConstraints = {@UniqueConstraint(columnNames = "name", name = "restaurants_unique_name_idx")})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Restaurant extends NamedEntity {
+}
