85e5c6f0fe5fc6e2703ffece0010d2e98e7475ea
ValeryManakov <valera.manakov.93@bk.ru>
2022-01-03 17:14:38 +0300

    6_add_dish_entity

---
+++ src/main/java/ru/javaops/topjava2/model/Dish.java
@@ -0,0 +1,30 @@
+package ru.javaops.topjava2.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "dishes", uniqueConstraints = {@UniqueConstraint(columnNames = {"restaurant_id", "name"}, name = "dishes_unique_restaurant_name_idx")})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Dish extends NamedEntity {
+
+    @Column(name = "price", nullable = false)
+    @NotNull
+    @Range(min = 10, max = 10000)
+    private Double price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Restaurant restaurant;
+}
