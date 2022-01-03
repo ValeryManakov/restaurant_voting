cfc64fccb155f2700d89ae9e2f6cf8b3ad958688
ValeryManakov <valera.manakov.93@bk.ru>
2022-01-03 10:25:14 +0300

    4_add_admin_restaurant_controller

---
+++ src/main/java/ru/javaops/topjava2/web/restaurant/AdminRestaurantController.java
@@ -0,0 +1,76 @@
+package ru.javaops.topjava2.web.restaurant;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.javaops.topjava2.model.Restaurant;
+import ru.javaops.topjava2.model.User;
+import ru.javaops.topjava2.repository.RestaurantRepository;
+import ru.javaops.topjava2.repository.UserRepository;
+import ru.javaops.topjava2.web.user.AdminUserController;
+
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+import static ru.javaops.topjava2.util.validation.ValidationUtil.assureIdConsistent;
+import static ru.javaops.topjava2.util.validation.ValidationUtil.checkNew;
+
+@RestController
+@RequestMapping(value = AdminRestaurantController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
+public class AdminRestaurantController {
+
+    static final String REST_URL = "/api/admin/restaurants";
+
+    @Autowired
+    private RestaurantRepository repository;
+
+    @GetMapping("/{id}")
+    public Restaurant get(@PathVariable int id) {
+        log.info("get {}", id);
+        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Restaurant '" + id + "' was not found"));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        log.info("delete {}", id);
+        repository.deleteExisted(id);
+    }
+
+    @GetMapping
+    public List<Restaurant> getAll() {
+        log.info("getAll");
+        return repository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Restaurant> createWithLocation(@Valid @RequestBody Restaurant restaurant) {
+        log.info("create {}", restaurant);
+        checkNew(restaurant);
+        Restaurant created = repository.save(restaurant);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@Valid @RequestBody Restaurant restaurant, @PathVariable int id) {
+        log.info("update {} with id={}", restaurant, id);
+        assureIdConsistent(restaurant, id);
+        repository.save(restaurant);
+    }
+}
