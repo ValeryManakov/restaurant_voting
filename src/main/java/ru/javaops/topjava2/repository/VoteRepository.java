6c2b7691f3a8c1d79581d3f75ad2a95c6eb0e05d
ValeryManakov <valera.manakov.93@bk.ru>
2022-01-04 19:29:41 +0300

    10_add_vote_repository

---
+++ src/main/java/ru/javaops/topjava2/repository/VoteRepository.java
@@ -0,0 +1,17 @@
+package ru.javaops.topjava2.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javaops.topjava2.model.Dish;
+import ru.javaops.topjava2.model.Vote;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+public interface VoteRepository extends BaseRepository<Vote> {
+
+    @Query("SELECT v FROM Vote v WHERE v.user.id = :userId AND v.registered = :registered")
+    Optional<Vote> findByUserId(int userId, LocalDate registered);
+}
