108f5f225bfe48424df4dbbe092f8d14d08660b6
ValeryManakov <valera.manakov.93@bk.ru>
2022-01-05 14:35:32 +0300

    17_add_abstract_restaurant_controller

--- src/main/java/ru/javaops/topjava2/web/restaurant/AdminRestaurantController.java
+++ src/main/java/ru/javaops/topjava2/web/restaurant/AdminRestaurantController.java
@@ -1,27 +1,16 @@
 package ru.javaops.topjava2.web.restaurant;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import ru.javaops.topjava2.error.IllegalRequestDataException;
 import ru.javaops.topjava2.model.Restaurant;
-import ru.javaops.topjava2.model.Vote;
-import ru.javaops.topjava2.repository.RestaurantRepository;
-import ru.javaops.topjava2.repository.VoteRepository;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.time.LocalDateTime;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import static ru.javaops.topjava2.util.validation.ValidationUtil.assureIdConsistent;
 import static ru.javaops.topjava2.util.validation.ValidationUtil.checkNew;
@@ -29,20 +18,14 @@ import static ru.javaops.topjava2.util.validation.ValidationUtil.checkNew;
 @RestController
 @RequestMapping(value = AdminRestaurantController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
-public class AdminRestaurantController {
+public class AdminRestaurantController extends AbstractRestaurantController {
 
     static final String REST_URL = "/api/admin/restaurants";
 
-    @Autowired
-    private RestaurantRepository restaurantRepository;
-
-    @Autowired
-    private VoteRepository voteRepository;
-
+    @Override
     @GetMapping("/{id}")
     public Restaurant get(@PathVariable int id) {
-        log.info("get {}", id);
-        return getRestaurant(id);
+        return super.get(id);
     }
 
     @DeleteMapping("/{id}")
@@ -52,10 +35,10 @@ public class AdminRestaurantController {
         restaurantRepository.deleteExisted(id);
     }
 
+    @Override
     @GetMapping
     public List<Restaurant> getAll() {
-        log.info("getAll");
-        return restaurantRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+        return super.getAll();
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -77,20 +60,9 @@ public class AdminRestaurantController {
         restaurantRepository.save(restaurant);
     }
 
+    @Override
     @GetMapping("/best-for-today")
     public List<Restaurant> getBest() {
-        log.info("getBest");
-        Map<String, Integer> votesCountPerRestaurant = voteRepository.findAll(Sort.by(Sort.Direction.DESC, "registered"))
-                .stream().filter(vote -> LocalDateTime.now().toLocalDate().equals(vote.getRegistered()))
-                .collect(Collectors.groupingBy(vote -> vote.getRestaurant().getName(), Collectors.summingInt(vote -> 1)));
-        Integer maxCount = votesCountPerRestaurant.values().stream()
-                .max(Comparator.comparingInt(count -> count)).orElse(null);
-        return votesCountPerRestaurant.keySet().stream()
-                .filter(name -> votesCountPerRestaurant.get(name).equals(maxCount))
-                .map(name -> restaurantRepository.getByName(name)).collect(Collectors.toList());
-    }
-
-    private Restaurant getRestaurant(int id) {
-        return restaurantRepository.findById(id).orElseThrow(() -> new IllegalRequestDataException("Restaurant '" + id + "' was not found"));
+        return super.getBest();
     }
 }
--- src/main/java/ru/javaops/topjava2/web/restaurant/ProfileRestaurantController.java
+++ src/main/java/ru/javaops/topjava2/web/restaurant/ProfileRestaurantController.java
@@ -22,42 +22,25 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping(value = ProfileRestaurantController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
-public class ProfileRestaurantController {
+public class ProfileRestaurantController extends AbstractRestaurantController {
 
     static final String REST_URL = "/api/profile/restaurants";
 
-    @Autowired
-    private RestaurantRepository restaurantRepository;
-
-    @Autowired
-    private VoteRepository voteRepository;
-
+    @Override
     @GetMapping("/{id}")
     public Restaurant get(@PathVariable int id) {
-        log.info("get {}", id);
-        return getRestaurant(id);
+        return super.get(id);
     }
 
+    @Override
     @GetMapping
     public List<Restaurant> getAll() {
-        log.info("getAll");
-        return restaurantRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+        return super.getAll();
     }
 
+    @Override
     @GetMapping("/best-for-today")
     public List<Restaurant> getBest() {
-        log.info("getBest");
-        Map<String, Integer> votesCountPerRestaurant = voteRepository.findAll(Sort.by(Sort.Direction.DESC, "registered"))
-                .stream().filter(vote -> LocalDateTime.now().toLocalDate().equals(vote.getRegistered()))
-                .collect(Collectors.groupingBy(vote -> vote.getRestaurant().getName(), Collectors.summingInt(vote -> 1)));
-        Integer maxCount = votesCountPerRestaurant.values().stream()
-                .max(Comparator.comparingInt(count -> count)).orElse(null);
-        return votesCountPerRestaurant.keySet().stream()
-                .filter(name -> votesCountPerRestaurant.get(name).equals(maxCount))
-                .map(name -> restaurantRepository.getByName(name)).collect(Collectors.toList());
-    }
-
-    private Restaurant getRestaurant(int id) {
-        return restaurantRepository.findById(id).orElseThrow(() -> new IllegalRequestDataException("Restaurant '" + id + "' was not found"));
+        return super.getBest();
     }
 }
